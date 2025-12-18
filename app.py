450d137ff416ed1a43ef6c305362f4898dc03663
Bhushan Chitte <bhushanrchitte2021@gmail.com>
2025-12-18 12:39:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from utils.info import Information
+
+path = "./data/crop_yield.csv"
+
+info = Information(path)
+info.get_info()
+
+
+
